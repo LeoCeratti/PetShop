c996d56cf774c1e22ae4d3bde1652a6f1a838b14
LeoCeratti <leonardo.ceratti@gmail.com>
2026-04-21 21:14:09 -0300

    Add files via upload

--- script.js
+++
@@ -1,5 +0,0 @@
- function agendar() {
-            let valor = document.getElementById("dataHora").value;
-            document.getElementById("resultado").innerText = 
-                "Agendado para: " + valor;
-        }
